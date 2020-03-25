7fd450a18f627742a7b939159b9e3f52ce8410d0
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-03-25 23:13:17 +0100

    create DiscoveryServer project close #1

--- DiscoveryServer/src/main/java/com/safa/discoveryserver/DiscoveryServerApplication.java
+++ DiscoveryServer/src/main/java/com/safa/discoveryserver/DiscoveryServerApplication.java
@@ -1,8 +1,8 @@
 package com.safa.discoveryserver;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
