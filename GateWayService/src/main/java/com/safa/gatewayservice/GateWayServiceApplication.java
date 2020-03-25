7f35580efc7aeb3184e527ce26f29d1fd37ba6e7
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-03-26 00:18:57 +0100

    init GateWayService project

---
+++ GateWayService/src/main/java/com/safa/gatewayservice/GateWayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.safa.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateWayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateWayServiceApplication.class, args);
+	}
+
+}
---
+++ GateWayService/src/test/java/com/safa/gatewayservice/GateWayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safa.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateWayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
