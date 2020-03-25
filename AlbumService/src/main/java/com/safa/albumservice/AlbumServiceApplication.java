98c5003714e8d65caabd9e9707bb997c503f6b77
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-03-26 00:54:15 +0100

    init  AlbumService project

---
+++ AlbumService/src/main/java/com/safa/albumservice/AlbumServiceApplication.java
@@ -0,0 +1,13 @@
+package com.safa.albumservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumServiceApplication.class, args);
+	}
+
+}
---
+++ AlbumService/src/test/java/com/safa/albumservice/AlbumServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safa.albumservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
