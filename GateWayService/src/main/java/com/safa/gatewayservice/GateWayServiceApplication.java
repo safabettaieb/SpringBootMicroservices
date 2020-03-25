bf8b4e0d5e12c2630ac049bf06e22cf4037046fd
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-03-26 00:42:16 +0100

    create GateWayService project close #2

--- GateWayService/src/main/java/com/safa/gatewayservice/GateWayServiceApplication.java
+++ GateWayService/src/main/java/com/safa/gatewayservice/GateWayServiceApplication.java
@@ -2,8 +2,12 @@ package com.safa.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class GateWayServiceApplication {
 
 	public static void main(String[] args) {
