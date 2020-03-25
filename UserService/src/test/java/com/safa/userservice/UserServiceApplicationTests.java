c95eb8f6ec3683a37aca7dcc3efb38e25fe82996
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-03-25 23:20:26 +0100

    init UserService project

---
+++ UserService/src/test/java/com/safa/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safa.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
