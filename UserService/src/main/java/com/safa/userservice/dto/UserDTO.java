310645e4b3d7b2e628b76ed52997ecc87df5a8b5
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-03-26 02:01:41 +0100

    create UserDTO

---
+++ UserService/src/main/java/com/safa/userservice/dto/UserDTO.java
@@ -0,0 +1,27 @@
+package com.safa.userservice.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+	
+	@Size(min = 3, max = 30,message = "name should have [3..30] chars")
+	private String name;
+
+	@Email(message = "email should have a valid email")
+	private String email;
+	
+	@Pattern(regexp = "$[0-9]{6-8}^")
+	private String password;
+
+
+
+}
